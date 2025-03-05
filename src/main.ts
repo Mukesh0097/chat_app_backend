16cd7c310dc377f1d8e224a311602192411b91a3
Mukesh Prajapat <113261568+Mukesh0097@users.noreply.github.com>
2025-03-05 11:54:34 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,88 @@
-import express, { Request } from "express";
-import { Server } from "socket.io";
-import http from "http";
-import cors from "cors";
-import dotenv from "dotenv";
+// import express, { Request } from "express";
+// import { Server } from "socket.io";
+// import http from "http";
+// import cors from "cors";
+// import dotenv from "dotenv";
 
-dotenv.config();
-
-
+// dotenv.config();
 
 
-const app = express();
-
-const users: string[] = []
-
-const httpServer = http.createServer(app);
 
 
-app.use(cors<Request>());
+// const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+// const users: string[] = []
+
+// const httpServer = http.createServer(app);
 
 
-app.get("/getuser", (req, res) => {
-    res.json(users)
-})
+// app.use(cors<Request>());
 
-const io = new Server(httpServer, {
-    cors: {
-        origin: "*",
-    }
-})
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// })
 
-io.use((socket, next) => {
-    const username = socket.handshake.auth.username;
-    if (!username) {
-        return next(new Error("invalid username"));
-    }
-    (socket as any).username = username;
-    next();
-});
 
-io.on("connection", (socket) => {
-    const users = [];
-    for (let [id, socket] of io.of("/").sockets) {
-        users.push({
-            userID: id,
-            username: (socket as any).username,
-        });
-    }
-    socket.emit("users", users);
+// app.get("/getuser", (req, res) => {
+//     res.json(users)
+// })
 
-    socket.on("privateMessage", ({ message, to }) => {
-        console.log(message, to);
-        socket.to(to).emit("private message", {
-            message,
-            from: socket.id,
-        });
-    });
+// const io = new Server(httpServer, {
+//     cors: {
+//         origin: "*",
+//     }
+// })
 
-    socket.broadcast.emit("user connected", {
-        userID: socket.id,
-        username: (socket as any).username,
-    });
-    // ...
-});
+// io.use((socket, next) => {
+//     const username = socket.handshake.auth.username;
+//     if (!username) {
+//         return next(new Error("invalid username"));
+//     }
+//     (socket as any).username = username;
+//     next();
+// });
 
-httpServer.listen(process.env.PORT, () => console.log("server is running on port 8000"));
+// io.on("connection", (socket) => {
+//     const users = [];
+//     for (let [id, socket] of io.of("/").sockets) {
+//         users.push({
+//             userID: id,
+//             username: (socket as any).username,
+//         });
+//     }
+//     socket.emit("users", users);
+
+//     socket.on("privateMessage", ({ message, to }) => {
+//         console.log(message, to);
+//         socket.to(to).emit("private message", {
+//             message,
+//             from: socket.id,
+//         });
+//     });
+
+//     socket.broadcast.emit("user connected", {
+//         userID: socket.id,
+//         username: (socket as any).username,
+//     });
+//     // ...
+// });
+
+// httpServer.listen(process.env.PORT, () => console.log("server is running on port 8000"));
+
+
+  import express, { Request, Response } from 'express'
+
+  const app = express()
+  const port = process.env.PORT || 8080
+
+  app.get('/', (_req: Request, res: Response) => {
+    return res.send('Express Typescript on Vercel')
+  })
+
+  app.get('/ping', (_req: Request, res: Response) => {
+    return res.send('pong 🏓')
+  })
+
+  app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`)
+  })
